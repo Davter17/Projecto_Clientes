760df59d1a7efbd00824cd8e696a8ff9cbd0de85
iñigo <iclaver90@gmail.com>
2023-12-08 12:22:03 +0100

    cambios clase Exoeriencias

--- js/Experiencia.js
+++ js/Experiencia.js
@@ -1,47 +1,47 @@
 
 // creamos la clase Experiencia
 
-class Experiencia{
+class Experiencia {
 
-constructor(autor,titulo,descripcion,fCreacion,imagen){
+    constructor(autor, titulo, descripcion,imagen, fCreacion) {
 
-    this._autor=autor;
-    this._titulo=titulo;
-    this._descripcion=descripcion,
-    this._imagen=imagen;
-    
-    this._fCreacion=new Date();
+        this._autor = autor;
+        this._titulo = titulo;
+        this._descripcion = descripcion,
+        this._imagen = imagen;
 
-}
+        this._fCreacion = fCreacion;
+
+    }
 
 
-// metodos getter
- get autor(){
-    return this._autor;
- }
-get titulo(){
-    return this._titulo;
-}
-get descripcion(){
-    return this._descripcion;
-}
-get imagen(){
-    return this._imagen;
-}
+    // metodos getter
+    get autor() {
+        return this._autor;
+    }
+    get titulo() {
+        return this._titulo;
+    }
+    get descripcion() {
+        return this._descripcion;
+    }
+    get imagen() {
+        return this._imagen;
+    }
 
-// metodos setter
- set autor(nombre){
-    this._autor=nombre;
- }
-set titulo(tit){
-    this._titulo=tit;
-}
-set descripcion(desc){
-    this._descripcion=desc;
-}
-set imagen(imgn){
-  this._imagen=imgn;  
-}
+    // metodos setter
+    set autor(nombre) {
+        this._autor = nombre;
+    }
+    set titulo(tit) {
+        this._titulo = tit;
+    }
+    set descripcion(desc) {
+        this._descripcion = desc;
+    }
+    set imagen(imgn) {
+        this._imagen = imgn;
+    }
 
 
 
@@ -53,20 +53,31 @@ set imagen(imgn){
 
 // creamos un array de Experiencias
 
-arrayExperiencias=[];
+arrayExperiencias = [];
 
 // funcion que crea nueva experiencia
-function nuevaExperiencia(){
+function nuevaExperiencia() {
 
-let titulo=document.getElementById("titulo_experiencia").value;
-let descripcion=document.getElementById("descripcion").value;
+    let autor = document.getElementById("autor_experiencia").value;
+    let titulo = document.getElementById("titulo_experiencia").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let imagen=document.getElementById("img_experiencia").value;
+    let fecha=fechaHoy();
 
-// sacar usuario del LS
-// let autor=
+    // sacar usuario del LS
+    // let autor=
+
+    let nuevaExp = new Experiencia(autor, titulo, descripcion,imagen, fecha)
+    arrayExperiencias.push(nuevaExp);
+    localStorage.setItem(titulo, JSON.stringify(nuevaExp));
+
+}
 
 
-let nuevaExp=new Experiencia(autor,titulo,descripcion,fCreacion,imagen)
-    
+
+function mostrarExperiencia(){
+
+
     
 
 
@@ -86,3 +97,4 @@ function fechaHoy() {
     let y = h.getFullYear();
     return fechaRegistro = d + "/" + m + "/" + y + "";
 }
+
